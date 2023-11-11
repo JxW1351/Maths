6417766016f0b3e8d51e53741a43b5b018051f98
JxW1351 <josephdward@hotmail.co.uk>
2023-11-11 12:37:07 +0000

    Updates euclideanAlgorithm to display all Highest Common Factors not just one

--- euclideanAlgorithm.hs
+++ euclideanAlgorithm.hs
@@ -1,6 +1,14 @@
-calc :: (Int,Int) -> (Int,Int) -> (Int,Int)
-calc a (0,0) = a
-calc a b = (calc b (q a b))
+calc :: (Int,Int) -> (Int,Int) -> [(Int,Int)]
+calc a b | x==0 = [(y,0),(0,y),(0,-y),(-y,0)]
+         | (x < 0)&&(y<0) = [(-x,-y),(-y,x),(x,y),(y,-x)]
+         | x<0 = [(y,-x),(-x,-y),(x,y),(-y,x)]
+         | y<0 = [(-y,x),(x,y),(-x,-y),(y,-x)]
+         | otherwise = [(x,y),(y,-x),(-y,x),(-x,-y)]
+        where (x,y) = euc a b
+
+euc :: (Int,Int) -> (Int,Int) -> (Int,Int)
+euc a (0,0) = a
+euc a b = (euc b (q a b))
 
 rdiv :: Int -> Int -> Int
 rdiv a b = round ((fromIntegral a) / (fromIntegral b))
@@ -12,13 +20,13 @@ modulo :: (Int,Int) -> (Int,Int) -> (Int,Int) -> (Int,Int)
 modulo (a,b) (c,d) (e,f) = ((a-(c*e - d*f)),(b-(c*f + d*e)))
 
 qm :: (Int, Int) -> (Int,Int) -> ((Int,Int),(Int,Int))
-qm a b = do 
+qm a b = do
   let x = (quotient a b)
   (x,modulo a b x)
 
 q :: (Int, Int) -> (Int,Int) -> (Int,Int)
-q a b = do 
+q a b = do
   let x = (quotient a b)
   modulo a b x
 
-help = "calc (a,b) (c,d) returns (e,f) such that e+fi is a Highest Common Factor of a+bi and c+di in the Gaussian Integers Z[i]"
+help = "calc (a,b) (c,d) returns a list of all pairs (e,f) such that e+fi is a Highest Common Factor of a+bi and c+di in the Gaussian Integers Z[i]"
