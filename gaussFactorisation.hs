8b7d031b5c80344528fcc92d12790f5caf38c605
JxW1351 <josephdward@hotmail.co.uk>
2023-11-26 11:30:02 +0000

    It's working :)

--- gaussFactorisation.hs
+++ gaussFactorisation.hs
@@ -1,12 +1,20 @@
-help = "Takes an input of (a,b) and returns a list of pairs (c,d) such that the list is a factorisation of a+bi into Irreducibles c+di in the Gaussian Integers Z[i] (CURRENTLY UNFINISHED)"
+help = "Takes an input of (a,b) and returns a list of pairs (c,d) such that the list is a factorisation of a+bi into Irreducibles c+di (with one unit to ensure the product is equal to the input) in the Gaussian Integers Z[i]"
 
-pretty :: (Int,Int) -> (Int,Int)
-pretty (x,y) | x<0 && y>0 = (y,-x)
-             | x<0 = (-x,-y)
-             | x==0 && y<0 = (-y,0)
-             | x==0 = (y,0)
-             | y<0 = (-y,x)
-             | otherwise = (x,y)
+pretty :: (Int,Int) -> (Int,Int) -- formats values with positive real part of greater modulus than the imaginary part
+pretty (x,y) | x>0 && x>=b = (x,y)
+             | x>0 && y>x = (y,-x)
+             | x>0 = (-y,x)
+             | x==0 && y>0 = (y,0)
+             | x==0 = (-y,0)
+             | y>0 && y>=a = (y,-x)
+             | y>0 = (-x,-y)
+             | b>=a = (-y,x)
+             | otherwise = (-x,-y)
+                where a | x<0 = -x
+                        | otherwise = x
+                      b | y<0 = -y
+                        | otherwise = y
+
 
 norm :: (Int,Int) -> Int
 norm (a,b) = a^2 + b^2
@@ -15,15 +23,14 @@ isPrime :: Int -> Bool
 isPrime 1 = False
 isPrime 0 = False
 isPrime x | x<0 = isPrime (-x)
-          | otherwise = length (filter (\c -> x`mod`c==0) [1..(floor(sqrt(fromIntegral x)))]) == 1
+          | otherwise = (primeFact x 1) == [(x,1)]
 
-primeFact :: Int -> [(Int,Int)]
-primeFact 1 = []
-primeFact n | isPrime n = [(n,1)]
-            | x`mod`4==3 = ((x,2*y): primeFact (n`div`(x^(2*y))))
-            | otherwise = ((x,y):primeFact (n`div`(x^y)))
-              where x = head(filter (\c -> n`mod`c==0) [2..])
-                    y = ord x n
+primeFact :: Int -> Int -> [(Int,Int)] -- returns [(x,y)] where x is prime and y is the greatest power of x that is a factor of n
+primeFact 1 _ = []
+primeFact n x | x^2 > n = [(n,1)]
+              | n`mod`x == 0 = ((x,y):primeFact (n`div`(x^y)) (x+1))
+              | otherwise = primeFact n (x+1)
+                     where y = ord x n
 
 ord :: Int -> Int -> Int
 ord 1 _ = 0
@@ -31,7 +38,7 @@ ord a n | n`mod`a == 0 = (ord a (n`div`a)) + 1
         | otherwise = 0
 
 
-fromNorm :: Int -> (Int,Int)
+fromNorm :: Int -> (Int,Int) -- returns a gaussian integer with norm n
 fromNorm n = euc (head (filter (\m -> (m^2) `mod` n == (n-1)) [1..(n`div`2)]),1) (n,0)
 
 fromNormWithConjugate :: (Int,Int) -> (Int,Int) -> [(Int,Int)]
@@ -56,7 +63,7 @@ calc (a,b) | mult x == (a,b) = x
              where x = calc2 (a,b)
 
 calc2 :: (Int,Int) -> [(Int,Int)]
-calc2 (a,b) = map pretty (concat (map (\x -> (fromNormWithConjugate x (a,b))) (primeFact (norm (a,b)))))
+calc2 (a,b) = map pretty (concat (map (\x -> (fromNormWithConjugate x (a,b))) (primeFact (norm (a,b)) 1)))
 
 mult :: [(Int,Int)] -> (Int,Int)
 mult (a:[]) = a
